60208db3edbca6f94c33c3e083bb21584f9fe07d
John Manack <jmnck28@gmail.com>
2021-03-19 15:01:00 -0400

    removes unused imports

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
 import Home from './components/Home'
 import ActivitiesList from './components/ActivitiesList';
 import { useState } from 'react';
-import { Tabs, Tab } from '@material-ui/core';
 import './App.css';
 
 function App() {
