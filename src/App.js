87f958ae02b97141d73a4f4231ce58ecd0f2a761
John Manack <jmnck28@gmail.com>
2021-03-19 10:40:29 -0400

    styles homepage nav bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
 import Home from './components/Home'
 import ActivitiesList from './components/ActivitiesList';
 import { useState } from 'react';
+import { Tabs, Tab } from '@material-ui/core';
 import './App.css';
 
 function App() {
